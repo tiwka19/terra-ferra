fa5291a8c79106ed052bcd2afd936f721510af51
somethingxdev <hevnxv19@gmail.com>
2023-10-15 16:09:35 +0200

    complete new pages

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,11 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js';
 // Підключення списку активних модулів
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js';
+import JustValidate from 'just-validate';
+
+const validator = new JustValidate('#form', {});
+validator
+  .addField('#name', [{ rule: 'required' }])
+  .addField('#email', [{ rule: 'required' }, { rule: 'email' }])
+  .addField('#phone', [{ rule: 'required' }, { rule: 'number' }]);
